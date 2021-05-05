03a26f7c20f85d1de37d4ddee5b0e9d683bd3120
Eric fillipe <ericfillipework@gmail.com>
2021-05-04 23:26:22 -0300

    * Concluindo [Desafio 01 - Utilizando recursão] * Alterando versão do elixir para 1.10

--- lib/list_lenght.ex
+++ lib/list_lenght.ex
@@ -1,5 +1,11 @@
 defmodule ListLenght do
-  def call() do
-    #TO DO
+  def call(list)  , do: sum(list,0)
+  defp sum([], acc) do
+    acc
+  end
+
+  defp sum([head | tail], acc) do
+    acc = 1 + acc
+    sum(tail, acc)
   end
 end
